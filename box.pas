910952be41e3285f6564a09501721d834653a19f
HELEN\Elena <lena_oles@mail.ru>
2021-02-01 00:17:05 +0300

    develop

---
+++ box.pas
@@ -0,0 +1,11 @@
+﻿Begin
+var a:integer;
+var b:integer;
+var c:integer;
+a := ReadInteger ('Целое число а: ');
+b := ReadInteger ('Целое число b: ');
+c:= a;
+a:= b;
+b:= c;
+Writeln ('now a = ', a, '; now b = ', b);
+End.
