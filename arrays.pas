4ee1a909a0b191cf97eef8449e37b51272895e8b
HELEN\Elena <lena_oles@mail.ru>
2021-02-03 19:58:48 +0300

    array for 5 user numbers

---
+++ arrays.pas
@@ -0,0 +1,11 @@
+﻿//спроси у пользователя 5 чисел и заполни ими массив
+begin
+  var myCoolArray: array [0..4] of real;  
+  for var x := 0 to 4 do
+  begin
+    var userNumber := ReadReal('Введите число ');
+    myCoolArray[x] := userNumber;
+  end;    
+  for var i := 0 to 4 do  
+    write(myCoolArray[i], ' ');  
+end.
