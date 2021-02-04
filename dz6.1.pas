ba0220c0847fb72f0cbc0ed93895904fdacc9908
HELEN\Elena <lena_oles@mail.ru>
2021-02-04 23:45:55 +0300

    upd: array and for loops variables are renamed; the same valiable is used in both for loops now; starting and ending values in for loops are changed.

--- dz6.1.pas
+++ dz6.1.pas
@@ -2,16 +2,16 @@
 //последовательно спросить эти числа и записать в массив
 //найти в массиве все числа, которые больше предыдущего
 begin
-  var arrayName: array [0..100] of real;  
+  var userNumbers: array [0..100] of real;  
   var numbersQty: Integer;
   repeat
     numbersQty := ReadInteger('Какое количество чисел в пределах 100 будут введены?')
   until numbersQty < 100;
-  for var x := 1 to numbersQty do  
-    arrayName[x] := ReadReal('Введите число: ');    
-  for var y := 2 to numbersQty do
+  for var j := 0 to numbersQty-1 do  
+    userNumbers[j] := ReadReal('Введите число: ');    
+  for var j := 1 to numbersQty-1 do
     begin
-  if arrayName[y] > arrayName[y - 1] then
-    write(arrayName[y], ' ');
+  if userNumbers[j] > userNumbers[j - 1] then
+    write(userNumbers[j], ' ');
   end;
 end.
