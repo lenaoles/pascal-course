19d5e8c0a2169d873955ecf0ebd1e85a25b5c217
HELEN\Elena <lena_oles@mail.ru>
2021-04-24 21:35:55 +0300

    trim upd, indexof

---
+++ dz_indexOf.pas
@@ -0,0 +1,32 @@
+﻿//надо сделать IndexOf,пользователь вводит две строки
+//надо найти позицию, с которой начинается вторая строка в первой
+//если попадания нет, то скажи пользователю что-нибудь плохое
+begin
+  var originLine := ReadString('Введите строку текста: ');   
+  var partOfOriginLine := ReadString('Введите часть строки которую нужно найти: ');
+  var index: integer;
+  
+  if partOfOriginLine.Length < 2 then
+    for var i := 1 to originLine.Length do  
+      if originLine[i] = partOfOriginLine[1] then
+      begin
+        index := i;
+        writeln(index);
+      end
+      else continue
+  else    
+  begin
+    for var y := 1 to originLine.Length do    
+      if originLine[y] = partOfOriginLine[1] then
+      begin
+        for var x := 1 to partOfOriginLine.Length - 1 do    
+          if originLine[y + x] = partOfOriginLine[x + 1] then
+          begin
+            index := y;
+            writeln(index);
+          end
+          else writeln('error')
+      end
+      else continue
+  end;
+end.
--- dz_trim.pas
+++ dz_trim.pas
@@ -1,16 +1,20 @@
 ﻿//сделать программу, которая будет работать как функция trim
 //пользователь вводит строку, а ты ему в ответ выводишь строку без пробелов по бокам
-//сделать программу, которая будет работать как функция trim
-//пользователь вводит строку, а ты ему в ответ выводишь строку без пробелов по бокам
 begin
   var originLine := ReadString('Введите строку текста: '); 
-  if originLine[1] = ' ' then 
-  begin
-    for var i := 1 to originLine.Length - 1 do
-      originLine[i] := (originLine[i + 1]);
-    delete(originLine, originLine.Length, 1);
-  end;
-  if(originLine[originLine.Length]) = ' ' then 
-    delete(originLine, originLine.Length, 1);
+  var begSpaceCount: Integer;
+  for var k := 1 to originLine.Length do
+    if originLine[k] = ' ' then
+      begSpaceCount := begSpaceCount + 1
+    else break;
+  //for var i := 1 to begSpaceCount do
+    //originLine[i] := (originLine[i + 1]);
+  delete(originLine, 1, begSpaceCount);  
+  var endSpaceCount: Integer;
+  for var l := originLine.Length downto 1 do
+    if originLine[l] = ' ' then
+      endSpaceCount := endSpaceCount + 1
+    else break;
+  delete(originLine, originLine.Length-endSpaceCount+1, endSpaceCount);
   writeln('Результат: ''', originLine, '''');
 end.
